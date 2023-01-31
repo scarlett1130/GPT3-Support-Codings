8a4b589c958c5e7efae5498916538c52dd8ddb0f
scarlett1130 <businessx.free2000@gmail.com>
2023-01-30 22:22:07 -0800

    completed project

--- client/script.js
+++ client/script.js
@@ -1,5 +1,5 @@
 import bot from './assets/bot.svg';
-import user from './assets/user.ico';
+import user from './assets/user.png';
 
 const form = document.querySelector('form');
 const chatContainer = document.querySelector('#chat_container');
